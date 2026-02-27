5d600267fc6a4c32ff76fc60d66c3b65d9413f5a
Soun Savdan <50708935+Savdancode@users.noreply.github.com>
2026-02-27 21:39:04 +0700

    update path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',   // IMPORTANT for local dev
+  base: '/mos-blog/',   // IMPORTANT for local dev
 })
